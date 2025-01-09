76631cee4e3d8e9ae9ed50aa315becdfc99c23b4
maratbabaev <you@example.com>
2025-01-09 16:33:42 +0300

    2 commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,21 @@ document.querySelectorAll("a").forEach((elem) => {
 		return false;
 	};
 });
+
+let burger = document.querySelector(".burger");
+let sidebar = document.querySelector(".sidebar");
+let main = document.querySelector(".main");
+
+burger.addEventListener("click", () => {
+	burger.classList.toggle("active");
+	sidebar.classList.toggle("active");
+	main.classList.toggle("bg-active");
+	document.body.classList.toggle("hidden");
+});
+
+main.addEventListener("click", () => {
+	burger.classList.remove("active");
+	sidebar.classList.remove("active");
+	main.classList.remove("bg-active");
+	document.body.classList.remove("hidden");
+});
